92e91654f9c586732b007c9c7d3f6ecaa50f575f
juanchos0412 <78168190+juanchos0412@users.noreply.github.com>
2021-10-15 23:08:01 -0500

    Delete finalizado directory

--- finalizado/backend_proyecto/src/database/db.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    db: "mongodb+srv://tentransportes:tentransportesbd@cluster0.a1ulp.mongodb.net/TentransportesBD?retryWrites=true&w=majority"
-     //db: "mongodb://localhost:27017/proyecto_g5"
-}
